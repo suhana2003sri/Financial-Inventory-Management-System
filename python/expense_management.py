d2e7024e1beba17570465ca853bd018b025c12a0
suhana2003sri <140141382+suhana2003sri@users.noreply.github.com>
2026-08-26 02:07:55 +0530

    Implement expense management

---
+++ python/expense_management.py
@@ -0,0 +1,8 @@
+def record_expense(expense_type, amount, description):
+    print("Expense recorded successfully.")
+    print("Expense Type:", expense_type)
+    print("Amount:", amount)
+    print("Description:", description)
+
+
+record_expense("Office Expenses", 5000, "Office supplies")
