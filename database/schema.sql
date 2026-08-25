0a63e62b339c418efb3f4723f1eff85a725c871e
suhana2003sri <140141382+suhana2003sri@users.noreply.github.com>
2026-08-26 01:28:06 +0530

    Create users table

--- database/schema.sql
+++ database/schema.sql
@@ -4,3 +4,10 @@
 CREATE DATABASE financial_inventory_management;
 
 USE financial_inventory_management;
+
+CREATE TABLE users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
